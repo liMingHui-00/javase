e767e7c4e1ac06dc0600408107d237f5afd54c87
liminghui <3209498857@qq.com>
2024-08-05 11:18:48 +0800

    多线程

--- src/day34/D1.java
+++ src/day34/D1.java
@@ -10,12 +10,22 @@ public class D1 implements Runnable{
         }
     }
 
-    private void sale() {
+//    private void sale() {
+//        synchronized (Thread.class){
+//            if (count<=0){
+//                flag = false;
+//                return;
+//            }
+//            System.out.println(Thread.currentThread().getName()+"窗口卖出了第"+count+"票");
+//            count--;
+//        }
+//    }
+private synchronized void sale() {
         if (count<=0){
             flag = false;
             return;
         }
         System.out.println(Thread.currentThread().getName()+"窗口卖出了第"+count+"票");
         count--;
-    }
+}
 }
