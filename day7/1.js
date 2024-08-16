e157bae6d943ac982f5e853c3f5a3269f4007a1f
liminghui <3209498857@qq.com>
2024-08-16 21:46:41 +0800

    js

---
+++ day7/1.js
@@ -0,0 +1,8 @@
+let str = "wq-ewq-ewqe"
+console.log(str.repeat(2))
+console.log(str.charAt(0))
+console.log(str.includes("wqe"))
+console.log(str.toLocaleLowerCase())
+console.log(str.toUpperCase())
+console.log(str.substring(0, 3))
+console.log(str.split("-"))
