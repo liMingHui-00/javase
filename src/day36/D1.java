0e3dad91341a95a3432ac1589b7047a937a720c9
liminghui <3209498857@qq.com>
2024-08-18 12:13:13 +0800

    string复习

--- src/day36/D1.java
+++ src/day36/D1.java
@@ -2,20 +2,39 @@ package day36;
 
 public class D1 {
     public static void main(String[] args) {
-        String arr[] = {"q", "w", "e", "r"};
-        String str = "";
-        for (int i = 0; i < arr.length; i++) {
-            if (i == 0) {
-                str = "[" + arr[i];
-            } else if (i <= arr.length - 1) {
-                str = str + "," + arr[i];
-            }
-            if (i == arr.length - 1) {
-                str = str + "]";
-            }
-
+//        String arr[] = {"q", "w", "e", "r"};
+//        String s = Arrays.toString(arr);
+//        System.out.println(s.charAt(0));
+//        for (int i  = s.length()-1; i >= 0; i--) {
+//            System.out.print(s.charAt(i));
+//        }
+//        String str = "abc";
+//        String str2 = "a"+"b"+"c";
+//        System.out.println(str.equals(str2));
+        StringBuilder sb = new StringBuilder();
+//        System.out.println(sb.capacity());
+//        for (int i = 0; i < 17; i++) {
+//            sb.append(i);
+//        }
+//        System.out.println(sb.capacity());
+        for (int i = 0; i < 35; i++) {
+            sb.append(i);
         }
-        System.out.println(str);
+        System.out.println(sb.length());
+        System.out.println(sb.capacity());
+//        String str = "";
+//        for (int i = 0; i < arr.length; i++) {
+//            if (i == 0) {
+//                str = "[" + arr[i];
+//            } else if (i <= arr.length - 1) {
+//                str = str + "," + arr[i];
+//            }
+//            if (i == arr.length - 1) {
+//                str = str + "]";
+//            }
+//
+//        }
+//        System.out.println(str);
 
     }
 }
