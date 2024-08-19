3005f238ef621c162e9ea23ecada35a8e6c775f8
liminghui <3209498857@qq.com>
2024-08-19 10:24:15 +0800

    js

--- day7/1.js
+++ day7/1.js
@@ -1,8 +1,14 @@
-let str = "wq-ewq-ewqe"
-console.log(str.repeat(2))
-console.log(str.charAt(0))
-console.log(str.includes("wqe"))
-console.log(str.toLocaleLowerCase())
-console.log(str.toUpperCase())
-console.log(str.substring(0, 3))
-console.log(str.split("-"))
+// let str = "wq-ewq-ewqe"
+// console.log(str.repeat(2))
+// console.log(str.charAt(0))
+// console.log(str.includes("wqe"))
+// console.log(str.toLocaleLowerCase())
+// console.log(str.toUpperCase())
+// console.log(str.substring(0, 3))
+// console.log(str.split("-"))
+function fn() {
+  console.log(1)
+}
+fn.prototype.age = 22
+fn.age = 11
+console.log(fn.age)
